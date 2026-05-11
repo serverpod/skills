54fe76cd49b555a726af103e80351b4b75de74bd
Jake Macdonald <jakemac@google.com>
2026-05-11 15:31:23 +0000

    document title param

--- lib/src/core/dialog_support.dart
+++ lib/src/core/dialog_support.dart
@@ -6,12 +6,16 @@ abstract interface class DialogSupport {
   ///
   /// Returns the index of the selected option, or null if the dialog was
   /// cancelled or not implemented.
+  /// 
+  /// The [title] will be shown in an implementation specific way if given.
   Future<int?> showSingleSelectDialog(List<String> options, {String? title});
 
   /// Shows a multi select dialog with the given [options].
   ///
   /// Returns the indices of the selected options, or null if the dialog was
   /// cancelled or not implemented.
+  /// 
+  /// The [title] will be shown in an implementation specific way if given.
   Future<Set<int>?> showMultiSelectDialog(List<String> options,
       {String? title});
 }
